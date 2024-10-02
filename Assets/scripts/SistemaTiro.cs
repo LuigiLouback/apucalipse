606b9e7263b5f273e9e8b54e29d72394f3554f4a
Luigi Louback de Oliveira <luigilouback@yahoo.com.br>
2024-10-02 19:13:31 -0300

    add atirar

--- Assets/scripts/SistemaTiro.cs
+++ Assets/scripts/SistemaTiro.cs
@@ -22,9 +22,13 @@ public class SistemaTiro : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
         mousePosi = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        
+        if (Input.GetMouseButton(0) && podeAtirar)
+        {
+            podeAtirar = false;
+            Instantiate(tiro, pontoDeFogo.position, pontoDeFogo.rotation);
+            Invoke("CDTiro", tempoEntreTiros);
+        }
     }
    
     private void FixedUpdate()
@@ -37,11 +41,20 @@ public class SistemaTiro : MonoBehaviour
         {
             srGun.flipX = false;  // Se o cursor está à direita, mantém a sprite normal
             transform.rotation = Quaternion.Euler(0, 0, angle);  // Rotação normal
+            pontoDeFogo.localPosition = new Vector2(Mathf.Abs(pontoDeFogo.localPosition.x), pontoDeFogo.localPosition.y); // Mantém o ponto de fogo na posição correta
+            pontoDeFogo.localRotation = Quaternion.Euler(0, 0, 270); // Mantém a rotação normal do ponto de fogo
         }
         else
         {
             srGun.flipX = true;   // Se o cursor está à esquerda, inverte a sprite
             transform.rotation = Quaternion.Euler(0, 0, angle + 180f);  // Ajusta a rotação ao inverter
+            pontoDeFogo.localPosition = new Vector2(-Mathf.Abs(pontoDeFogo.localPosition.x), pontoDeFogo.localPosition.y); // Inverte a posição do ponto de fogo
+            pontoDeFogo.localRotation = Quaternion.Euler(0, 0, 90f); // Ajusta a rotação do ponto de fogo
         }
+    }
+
+    private void CDTiro()
+    {
+        podeAtirar = true;
     }
 }
