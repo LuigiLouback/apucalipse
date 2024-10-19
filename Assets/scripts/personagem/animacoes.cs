1b8eb8e489acded0b4f006bd38c62f25abdf53b5
Luigi Louback de Oliveira <luigilouback@yahoo.com.br>
2024-10-19 13:45:21 -0300

    add dust trail

--- Assets/scripts/personagem/animacoes.cs
+++ Assets/scripts/personagem/animacoes.cs
@@ -2,26 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class animacoes : MonoBehaviour
+public class Animacoes : MonoBehaviour
 {
-    
+    public ParticleSystem dust;
     public Animator animator;
     public Rigidbody2D rigidbody2d;
     public SpriteRenderer spriteRenderer;
 
+    private bool facingRight = true; // Variável para armazenar a direção atual
+
     // Update is called once per frame
     void Update()
     {
-       Vector2 velocidade = this.rigidbody2d.velocity;
-       if((velocidade.x != 0)||(velocidade.y != 0)){
-        this.animator.SetBool("correndodir",true);
-       }else{
-        this.animator.SetBool("correndodir",false);
-       }
-       if(velocidade.x > 0){
-        this.spriteRenderer.flipX=false;
-       }else if(velocidade.x < 0){
-        this.spriteRenderer.flipX=true;
-       }
+        Vector2 velocidade = this.rigidbody2d.velocity;
+        if ((velocidade.x != 0) || (velocidade.y != 0))
+        {
+            this.animator.SetBool("correndodir", true);
+        }
+        else
+        {
+            this.animator.SetBool("correndodir", false);
+        }
+
+        if (velocidade.x > 0 && !facingRight)
+        {
+            FlipSprite(false);
+        }
+        else if (velocidade.x < 0 && facingRight)
+        {
+            FlipSprite(true);
+        }
+    }
+
+    private void FlipSprite(bool flip)
+    {
+        facingRight = !flip;
+        spriteRenderer.flipX = flip;
+        dust.Play();
     }
 }
